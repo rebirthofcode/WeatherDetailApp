5f20a5b50d9d83529b813adbf34de77af03aea42
Janiré Castillo <hello@articulategeek.com>
2019-05-15 20:07:55 -0400

    Hello Swift

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Janiré Castillo on 5/15/19.
+//  Copyright © 2019 Janiré Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+         print("Hello World")
+    }
+}
+
